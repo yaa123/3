086f02ce4992e116a41031dbe518e36eccf4bf3d
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-01 21:20:31 +0500

    '2'

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@
 import telebot
 from telebot import types
 from telebot.types import Message
-
 TOKEN = '798979267:AAEWbRlo36GcdtkvhM7AQ8psNpnx041nWUk'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message):
+@bot.message_handler(content_types=['text'])
+def echo_digits(message: Message):
+bot.reply_to(message, 'test')
 	pass
 
 
