bfe0935f03066ede9786d51fb36581d934e4a8c3
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-01 21:06:07 +0500

    '2'

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ TOKEN = '798979267:AAEWbRlo36GcdtkvhM7AQ8psNpnx041nWUk'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
-def command_handler(message: Message):
-    bot.reply_to(message, 'Привет')
+def handle_start_help(message):
+	pass
 
 
 bot.polling(timeout=60)
