47891048e02553b5de3292edb28b051b694d5473
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-01 22:57:35 +0500

    '2'

--- bot.py
+++ bot.py
@@ -13,14 +13,16 @@ def command_handler(message):
 def command_handler(message):
     bot.reply_to(message, 'Помощь')
 	
+@bot.message_handler(commands=['ошибка'])
+def error (message):
+	bot.reply_to(message, 'ошибка')
+	
 @bot.message_handler(content_types=['text'])
 @bot.edited_message_handler(content_types=['text'])
 def echo_digits(message: Message):
 	bot.reply_to(message, 'test')	
 
-@bot.message_handler(commands=['ошибка'])
-def error (message):
-	bot.reply_to(message, 'ошибка')
+
 	
 	
 bot.polling(timeout=60)
