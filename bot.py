c9ca76669f1d6ceba1c7840034aa8e0d735d7bc8
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-01 21:44:41 +0500

    '2'

--- bot.py
+++ bot.py
@@ -5,10 +5,16 @@ from telebot.types import Message
 TOKEN = '798979267:AAEWbRlo36GcdtkvhM7AQ8psNpnx041nWUk'
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def command_handler(message: Message):
+    bot.reply_to(message, 'Здравствуйте! Этот Бот создан для помощи ннженерам АТМ_Альянс')
+
+@bot.message_handler(commands=['help'])
+def command_handler(message: Message):
+    bot.reply_to(message, 'Помощь')
+	
 @bot.message_handler(content_types=['text'])
 def echo_digits(message: Message):
-	bot.reply_to(message, 'test')
-	return
-
+	bot.reply_to(message, 'test')	
 
 bot.polling(timeout=60)
