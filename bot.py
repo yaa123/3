fc231331332972eae832ae4732d66d2270885e95
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-05 22:52:34 +0500

    '1'

--- bot.py
+++ bot.py
@@ -4,19 +4,21 @@ TOKEN = '798979267:AAEWbRlo36GcdtkvhM7AQ8psNpnx041nWUk'
 bot = telebot.TeleBot(TOKEN)
 inputfile = 'errorcode.txt'
 myfile = open(inputfile, mode='r', encoding='UTF-8')
-@bot.message_handler(commands=['start'])
+
 def file(text):
 	for line in myfile:
 		if text in line:
 			err = line.strip()
 			return err
 
-			
+@bot.message_handler(commands=['start'])	
 def command_handler(message):
 	bot.reply_to(message, 'Здравствуйте! Этот Бот создан для помощи ннженерам АТМ_Альянс')
+	
 @bot.message_handler(commands=['help'])
 def command_handler(message):
 	bot.reply_to(message, 'Помощь')
+	
 @bot.message_handler(content_types=['text'])
 @bot.edited_message_handler(content_types=['text'])
 def echo_digits(message):
