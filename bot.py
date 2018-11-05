e9a60bcccc68a1d4e71e7fd7258b4181ccf53b4e
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-05 20:22:27 +0500

    '1'

--- bot.py
+++ bot.py
@@ -10,24 +10,23 @@ myfile = open(inputfile, mode='r', encoding='UTF-8')
 @bot.message_handler(commands=['start'])
 def command_handler(message):
 	bot.reply_to(message, 'Здравствуйте! Этот Бот создан для помощи ннженерам АТМ_Альянс')
-	pass
-	
+
 @bot.message_handler(commands=['help'])
 def command_handler(message):
 	bot.reply_to(message, 'Помощь')
-	pass
-	
+
+
 @bot.message_handler(commands=['error'])
 def error (message):
 	for line in myfile:
 		if text in line:
 			bot.reply_to(message, 'ошибка' + line.strip())
-			pass
-		
+
+
 @bot.message_handler(content_types=['text'])
 @bot.edited_message_handler(content_types=['text'])
 def echo_digits(message: Message):
 	bot.reply_to(message, 'test')	
-	pass
-	
+
+
 bot.polling(timeout=60)
