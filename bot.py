6ff97cf47f2d4cca805ec5d6790e2ab3beb18652
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-06 13:52:43 +0500

    '1'

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import telebot
 TOKEN = '798979267:AAEWbRlo36GcdtkvhM7AQ8psNpnx041nWUk'
 bot = telebot.TeleBot(TOKEN)
 inputfile = 'errorcode.txt'
-#sticker_id = 'CAADAgADAQAD0VrUCH5Dfvp5fahZAg'
+sticker_id = 'CAADAgADAQAD0VrUCH5Dfvp5fahZAg'
 def file(text):
 	myfile = open(inputfile, mode='r', encoding='UTF-8')
 	for line in myfile:
@@ -24,9 +24,9 @@ def command_handler(message):
 def command_handler(message):
 	bot.reply_to(message, 'Отправьте мне код ошибки, чтобы получить описание')
 
-#@bot.message_handler(commands=['edik'])
-#def command_handler(message):
-#	bot.reply_to(message, sticker_id)
+@bot.message_handler(commands=['edik'])
+def command_handler(message):
+	bot.send_sticker(message.chat.id, sticker_id)
 
 @bot.message_handler(content_types=['sticker'])
 def test(message):
