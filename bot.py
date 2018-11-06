13f9812d788f2dc2752553354b1c865208f75e37
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-06 13:31:59 +0500

    '1'

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ def command_handler(message):
 	bot.reply_to(message, 'Отправьте мне код ошибки, чтобы получить описание')
 
 @bot.message_handler(commands=['edik'])
-def command_handler(message):
-	bot.reply_to(message.sticker, sticker_id)
+def command_handler(message: Message):
+	bot.reply_to(message, sticker_id)
 
 @bot.message_handler(content_types=['sticker'])
 def test(message):
