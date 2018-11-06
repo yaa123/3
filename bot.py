681f6a0c4ad27eee78c114f5d6e4cd3c68a565d7
yaa123 <44618437+yaa123@users.noreply.github.com>
2018-11-06 14:36:09 +0500

    '1'

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def echo_digits(message):
 	text = file(message.text.lower())
 	bot.reply_to(message, ('Ошибка ' + text))
 
-@bot.inline_handler(inline query: query)
+@bot.inline_handler(content_types=['text'])
 def query_text(inline_query):
 	text = file(message.text.lower())
 	bot.answer_inline_query(inline_query.id, text, {cache_time: 0})
